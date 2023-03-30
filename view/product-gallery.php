c60c282125c27bc55a333a88e99ffebad850a835
Lydia Dereje <lydia@Lydias-MacBook-Air-3.local>
2023-03-30 12:02:44 -0400

    billing details and shopping cart

---
+++ view/product-gallery.php
@@ -0,0 +1,76 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+       
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+
+
+<div class="container">
+          <div class="row">
+            <div class="col">
+              <div class="card text-center">
+                <div class="card-header">
+                  Basic
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Package Name 1</h5>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Line 1</li>
+                    <li class="list-group-item">Line 2</li>
+                  </ul>
+                  <button onclick="cartAction('add','code1','Package 1',10)" href="/billing-details.php" class="btn btn-primary">Purchase</button>
+                </div>
+                <div class="card-footer text-muted">
+                  Best for those who do not want a huge wedding.
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card text-center">
+                <div class="card-header">
+                  Intermediate
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Package Name 2</h5>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Line 1</li>
+                    <li class="list-group-item">Line 2</li>
+                    <li class="list-group-item">Line 3</li>
+                    <li class="list-group-item">Line 4</li>
+                  </ul>
+                  <button onclick="cartAction('add','code2','Package 2',20)" href="/billing-details.php" class="btn btn-primary">Purchase</bu>
+                </div>
+                <div class="card-footer text-muted">
+                  A well-rounded package best for a budget.
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card text-center">
+                <div class="card-header">
+                  Advanced
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Package Name 3</h5>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Line 1</li>
+                    <li class="list-group-item">Line 2</li>
+                    <li class="list-group-item">Line 3</li>
+                    <li class="list-group-item">Line 4</li>
+                    <li class="list-group-item">Line 5</li>
+                    <li class="list-group-item">Line 6</li>
+                  </ul>
+                  <button onclick="cartAction('add','code2','Package 3',30)" href="/billing-details.php" class="btn btn-primary">Purchase</button>
+                </div>
+                <div class="card-footer text-muted">
+                  Best for the ultimate experience.
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
