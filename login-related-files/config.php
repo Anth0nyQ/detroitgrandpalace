e90143cef01b7385c92cf3d84113837bec81afab
Di4betus <106601448+Di4betus@users.noreply.github.com>
2023-04-17 18:58:25 -0400

    Fixed known issues.
    Fixed missing picture + added home page.

--- login-related-files/config.php
+++ login-related-files/config.php
@@ -5,7 +5,7 @@ $password = "";
 $db_name = "mysql:host=localhost;dbname=login";
 
 $conn = new PDO($db_name,$username, $password);
-	 /*
+	/*
 	// Check connection 
 	if ($conn->connect_error) { 
 	    die("Connection failed: " . $conn->connect_error); 
